639e15c061f27be4127e017c0e4a292955cf1bb3
liuzx <liuzx@chinasofti.com>
2017-08-18 00:55:26 +0800

    init server

---
+++ server-parent/server-consumer/src/main/java/org/server/consumer/netty/NettyServerHandler.java
@@ -0,0 +1,54 @@
+package org.server.consumer.netty;
+
+import org.common.utils.transport.CustomMsg;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class NettyServerHandler extends SimpleChannelInboundHandler<Object> {
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		System.out.println("exceptionCaught ");
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+	/*@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+		System.out.println("channelRead : " + msg);
+		
+		 * ByteBuf buf = (ByteBuf) msg; byte[] req = new
+		 * byte[buf.readableBytes()]; buf.readBytes(req); String body = new
+		 * String(req, "UTF-8"); System.out.println("the time server receive:" +
+		 * body); // 接收到数据 String resp = " 返回数据：" + System.currentTimeMillis();
+		 * ByteBuf respBuf = Unpooled.copiedBuffer(resp.getBytes("UTF-8"));
+		 * ctx.write(respBuf);
+		 
+	}*/
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+		// System.out.println("channelReadComplete ");
+		ctx.flush();
+	}
+
+	@Override
+	protected void messageReceived(ChannelHandlerContext ctx, Object msg) throws Exception {
+		if (msg instanceof CustomMsg) {
+			CustomMsg customMsg = (CustomMsg) msg;
+			//TODO 把数据放在mongodb
+			
+			System.out.println("NettyServer:" + ctx.channel().remoteAddress() + " send " + customMsg.getBody());
+		} else {
+			ByteBuf buf = (ByteBuf) msg;
+			byte[] req = new byte[buf.readableBytes()];
+			buf.readBytes(req);
+			String body = new String(req, "UTF-8");
+			System.out.println("the time server receive:" + body);
+		}
+
+	}
+
+}
